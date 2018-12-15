28bc8b135af9c2eefb0677ea21eb05993aa21260
Ahmad Alhashemi <ahmad@ahmadh.com>
2018-12-15 19:03:10 +0300

    first working VM

--- Sources/lc3-swift/main.swift
+++ Sources/lc3-swift/main.swift
@@ -1,1 +1,336 @@
-print("Hello, world!")
+import Darwin
+
+let memory = UnsafeMutableBufferPointer<UInt16>.allocate(capacity: Int(UInt16.max))
+
+enum Register: UInt8, CaseIterable {
+    case r0, r1, r2, r3, r4, r5, r6, r7,
+         pc, cond
+}
+
+struct RegisterSet {
+    let storage = UnsafeMutableBufferPointer<UInt16>.allocate(capacity: Register.allCases.count)
+    
+    subscript(r: Register) -> UInt16 {
+        get {
+            return storage[Int(r.rawValue)]
+        }
+        
+        set {
+            storage[Int(r.rawValue)] = newValue
+        }
+    }
+    
+    subscript(_ rn: UInt16) -> UInt16 {
+        get {
+            return storage[Int(rn)]
+        }
+        
+        set {
+            storage[Int(rn)] = newValue
+        }
+    }
+}
+
+var reg = RegisterSet()
+
+enum OpCode: UInt16 {
+    case br, add, ld, st, jsr, and, ldr, str, rti, not,
+         ldi, sti, jmp, res, lea, trap
+}
+
+enum TrapCode: UInt16 {
+    case getc  = 0x20 // get character from keyboard
+    case out   = 0x21 // output a character
+    case puts  = 0x22 // output a word string
+    case `in`  = 0x23 // input a string
+    case putsp = 0x24 // output a byte string
+    case halt  = 0x25 // halt the program
+}
+
+extension UInt16 {
+    static var pos: UInt16 { return (1 << 0) }
+    static var zro: UInt16 { return (1 << 1) }
+    static var neg: UInt16 { return (1 << 2) }
+}
+
+func sign_extend(_ x: UInt16, bit_count: UInt16) -> UInt16 {
+    if ((x >> (bit_count - 1)) & UInt16(1)) != 0 {
+        return x | (0xFFFF << bit_count)
+    } else {
+        return x
+    }
+}
+
+func update_flags(_ r: UInt16) {
+    if reg[r] == 0 {
+        reg[.cond] = .zro
+    } else if (reg[r] >> 15) == 1 {
+        reg[.cond] = .neg
+    } else {
+        reg[.cond] = .pos
+    }
+}
+
+let PC_START: UInt16 = 0x3000
+reg[.pc] = PC_START
+
+func mem_write(_ address: UInt16, _ val: UInt16) {
+    memory[Int(address)] = val
+}
+
+struct MR {
+    static let KBSR: UInt16 = 0xFE00 // keyboard status
+    static let KBDR: UInt16 = 0xFE02 // keyboard data
+}
+
+func mem_read(_ address: UInt16) -> UInt16 {
+    if address == MR.KBSR {
+        if check_key() {
+            memory[Int(MR.KBSR)] = (1 << 15)
+            memory[Int(MR.KBDR)] = UInt16(getchar())
+        } else {
+            memory[Int(MR.KBSR)] = 0
+        }
+    }
+    
+    return memory[Int(address)]
+}
+
+
+// Ports the FD_ZERO and FD_SET C macros in Darwin to Swift.
+// https://github.com/apple/darwin-xnu/blob/master/bsd/sys/_types/_fd_def.h
+// This is not portable to Linux.
+extension fd_set {
+    // FD_ZERO(self)
+    mutating func fdZero() {
+        bzero(&fds_bits, MemoryLayout.size(ofValue: fds_bits))
+    }
+    
+    // FD_SET(fd, self)
+    mutating func fdSet(fd: Int32) {
+        let __DARWIN_NFDBITS = Int32(MemoryLayout<Int32>.size) * __DARWIN_NBBY
+        let bits = UnsafeMutableBufferPointer(start: &fds_bits.0, count: 32)
+        bits[Int(CUnsignedLong(fd) / CUnsignedLong(__DARWIN_NFDBITS))] |= __int32_t(
+            CUnsignedLong(1) << CUnsignedLong(fd % __DARWIN_NFDBITS)
+        )
+    }
+}
+
+/*
+__DARWIN_FD_SET(n, p)    do {
+    int __fd = (n);
+    (fds_bits[(unsigned long)__fd/__DARWIN_NFDBITS] |=
+        (
+            (__int32_t)(
+                (
+                   (unsigned long)1
+                ) << (
+                   (unsigned long)__fd % __DARWIN_NFDBITS
+                )
+            )
+        )
+    );
+} while(0)
+*/
+
+func check_key() -> Bool {
+    var readfds = fd_set()
+    readfds.fdZero()
+    readfds.fdSet(fd: STDIN_FILENO)
+    
+    var timeout = timeval(tv_sec: 0, tv_usec: 0)
+    return select(1, &readfds, nil, nil, &timeout) != 0
+}
+
+func run() {
+    var running = true
+    
+    func handleTrap(_ instr: UInt16) {
+        let trapCode = TrapCode.init(rawValue: instr & 0xFF)!
+        
+        switch trapCode {
+        case .getc:
+            reg[.r0] = UInt16(getchar())
+            
+        case .out:
+            putc(Int32(reg[.r0]), stdout)
+            fflush(stdout)
+            
+        case .puts:
+            var idx = Int(reg[.r0])
+            while memory[idx] != 0 {
+                putc(Int32(memory[idx]), stdout)
+                idx += 1
+            }
+            fflush(stdout)
+            
+        case .in:
+            print("Enter a character: ", terminator: "")
+            reg[.r0] = UInt16(getchar())
+            
+        case .putsp:
+            var idx = Int(reg[.r0])
+            while memory[idx] != 0 {
+                let char1 = Int32(memory[idx] & 0xFF)
+                putc(char1, stdout)
+                let char2 = Int32(memory[idx] >> 8)
+                if char2 != 0 { putc(char2, stdout) }
+                idx += 1
+            }
+            fflush(stdout)
+            
+        case .halt:
+            puts("HALT")
+            fflush(stdout)
+            running = false
+        }
+    }
+    
+    while running {
+        /* FETCH */
+        let instr = mem_read(reg[.pc])
+        reg[.pc] += 1
+        guard let op = OpCode.init(rawValue: instr >> 12) else {
+            fatalError("bad opcode")
+        }
+        
+        var r0: UInt16 { return (instr >> 9) & 0x7 }
+        var r1: UInt16 { return (instr >> 6) & 0x7 }
+        var imm_flag: Bool { return ((instr >> 5) & 0x1) == 1 }
+        var imm5: UInt16 { return sign_extend(instr & 0x1F, bit_count: 5) }
+        var r2: UInt16 { return instr & 0x7 }
+        var pc_offset: UInt16 { return sign_extend(instr & 0x1ff, bit_count: 9) }
+        var long_pc_offset: UInt16 { return sign_extend(instr & 0x7ff, bit_count: 11) }
+        var long_flag: Bool { return ((instr >> 11) & 1) != 0 }
+        var offset: UInt16 { return sign_extend(instr & 0x3F, bit_count: 6) }
+        
+        switch op {
+        case .add:
+            reg[r0] = reg[r1] &+ (imm_flag ? imm5 : reg[r2])
+            update_flags(r0)
+            
+        case .and:
+            reg[r0] = reg[r1] & (imm_flag ? imm5 : reg[r2])
+            update_flags(r0)
+            
+        case .not:
+            reg[r0] = ~reg[r1]
+            update_flags(r0)
+        
+        case .br:
+            let cond_flag = r0
+            if (cond_flag & reg[.cond]) != 0 {
+                reg[.pc] &+= pc_offset
+            }
+        
+        case .jmp:
+            reg[.pc] = reg[r1]
+            
+        case .jsr:
+            reg[.r7] = reg[.pc]
+            if long_flag {
+                reg[.pc] &+= long_pc_offset // JSR
+            } else {
+                reg[.pc] = reg[r1] // JSRR
+            }
+        
+        case .ld:
+            reg[r0] = mem_read(reg[.pc] &+ pc_offset)
+            update_flags(r0)
+        
+        case .ldr:
+            reg[r0] = mem_read(reg[r1] &+ offset)
+            update_flags(r0)
+        
+        case .lea:
+            reg[r0] = reg[.pc] &+ pc_offset
+            update_flags(r0)
+        
+        case .st:
+            mem_write(reg[.pc] &+ pc_offset, reg[r0])
+        
+        case .sti:
+            mem_write(mem_read(reg[.pc] &+ pc_offset), reg[r0])
+        
+        case .str:
+            mem_write(reg[r1] &+ offset, reg[r0])
+        
+        case .trap:
+            handleTrap(instr)
+
+        case .ldi:
+            reg[r0] = mem_read(mem_read(reg[.pc] &+ pc_offset))
+            update_flags(r0)
+            
+        case .rti: fallthrough
+        case .res:
+            fatalError("bad opcode")
+        }
+    }
+}
+
+extension UInt16 {
+    var swap16: UInt16 {
+        return (self << 8) | (self >> 8)
+    }
+}
+
+func read_image_file(_ file: UnsafeMutablePointer<FILE>) {
+    var origin: UInt16 = 0
+    fread(&origin, MemoryLayout.size(ofValue: origin), 1, file)
+    origin = origin.swap16
+    
+    let max_read = UInt16.max - origin
+    let rebased = UnsafeMutableBufferPointer(rebasing: memory[Int(origin)...])
+    let read = fread(rebased.baseAddress!, MemoryLayout.size(ofValue: origin), Int(max_read), file)
+    
+    for idx in Int(origin)..<Int(origin) + read {
+        //print(memory[idx])
+        memory[idx] = memory[idx].swap16
+    }
+}
+
+func read_image(_ image_path: String) -> Bool {
+    guard let file = fopen(image_path, "rb") else { return false }
+    read_image_file(file)
+    fclose(file)
+    return true
+}
+
+// main
+if CommandLine.argc < 2 {
+    print("\(CommandLine.arguments[0]) [image-file1] ...")
+    exit(2)
+}
+
+for arg in CommandLine.arguments[1...] {
+    guard read_image(arg) else {
+        fatalError("failed to load image: \(arg)")
+    }
+}
+
+var original_tio = termios()
+
+func disable_input_buffering() {
+    tcgetattr(STDIN_FILENO, &original_tio)
+    var new_tio = original_tio
+    new_tio.c_lflag = UInt(Int32(new_tio.c_lflag) & ~ICANON & ~ECHO)
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio)
+}
+
+func restore_input_buffering() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio)
+}
+
+func handle_interrupt(_ signal: Int32) {
+    restore_input_buffering()
+    print("")
+    exit(-2)
+}
+
+signal(SIGINT, handle_interrupt)
+disable_input_buffering()
+
+run()
+
+restore_input_buffering()
